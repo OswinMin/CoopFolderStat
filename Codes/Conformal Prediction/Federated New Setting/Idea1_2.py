caeb5d3113edf696bd36037baf9f7b9d298a59f6
oswinmin <oswinmin@gmail.com>
2024-05-29 17:33:41 +1000

    Idea2 推导尝试

--- Codes/Conformal Prediction/Federated New Setting/Idea1_2.py
+++ Codes/Conformal Prediction/Federated New Setting/Idea1_2.py
@@ -7,6 +7,8 @@ from torch.utils.data import DataLoader, TensorDataset
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import hashlib
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
 
 h = 0.5
 
@@ -100,7 +102,53 @@ def Test(X0:np.ndarray, X, Y, theta, predictor):
     for i in range(len(X0)):
         weight = Weight(Xtilde[i], X, h)
         Qtilde = ECDFQ(Score.reshape(-1), weight.reshape(-1), 0.1)
-        Conf1[:,i] = Conf(Y0hat[i], Qtilde)
-        P[i] = stats.norm.cdf(Conf1[1,i], loc=X0[i]**2, scale=ep*np.abs(X0[i]))\
-            - stats.norm.cdf(Conf1[0,i], loc=X0[i]**2, scale=ep*np.abs(X0[i]))
-    return Conf0, Conf1, P
+        lowerbound1[i], upperbound1[i] = Conf(Y0hat[i], Qtilde)
+        P[i] = stats.norm.cdf(lowerbound1[i], loc=X0[i]**2, scale=ep*(np.abs(X0[i])+theta[0]))\
+            - stats.norm.cdf(upperbound1[i], loc=X0[i]**2, scale=ep*(np.abs(X0[i])+theta[0]))
+    return lowerbound0, upperbound0, lowerbound1, upperbound1, P
+
+def Draw(X, Y, X0, loc, theta, lowerbound0, upperbound0, lowerbound1, upperbound1):
+    cmap = cm.get_cmap('cividis')
+    st = f"loc: " + ', '.join([str(i) for i in loc]) + f';  num: {num}' + '\ntheta: ' + \
+         ', '.join([str(round(i, 1)) for i in theta])
+    plt.figure(figsize=(8,6))
+    if len(X.shape)==1:
+        X_ = X.reshape((-1,1))
+        Y_ = Y.reshape((-1,1))
+    else:
+        X_ = X
+        Y_ = Y
+    norm = mcolors.Normalize(vmin=-X_.shape[1]-1, vmax=X_.shape[1]-1)
+    for j in range(X_.shape[1]):
+        x_ = X_[:,j][(X_[:,j]>min(X0))&(X_[:,j]<max(X0))]
+        y_ = Y_[:,j][(X_[:,j]>min(X0))&(X_[:,j]<max(X0))]
+        plt.scatter(x_, y_, s=3, label=f"loc: {loc[j]}", color=cmap(norm(j)))
+    plt.plot(X0, lowerbound0, color='red', label='True')
+    plt.plot(X0, upperbound0, color='red')
+    plt.plot(X0, lowerbound1, color='blue', label='Estimated')
+    plt.plot(X0, upperbound1, color='blue')
+    plt.legend()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title(st)
+    plt.savefig(f"resultFig/Idea1_2/{sha256(st)[:5]}.png", dpi=400)
+    plt.show()
+
+if __name__ == '__main__':
+    h = 0.5
+    loc = [0, 20]
+    num = 500
+    X, Y, theta = Xshift(num, loc, fun)
+    predictor = Predictor([18, 9])
+    predictor.train(X.reshape(-1, 1), Y.reshape(-1, 1), epochs=300)
+    X0 = np.linspace(-4, 4, 500)
+    lowerbound0, upperbound0, lowerbound1, upperbound1, P = Test(X0, X, Y, theta, predictor)
+    Draw(X, Y, X0, loc, theta, lowerbound0, upperbound0, lowerbound1, upperbound1)
+
+    ### 用上面生成的 X 第一列
+    X_, Y_ = X[:, [0]], Y[:, [0]]
+    X0 = np.linspace(-4, 4, 500)
+    predictor = Predictor([15, 7])
+    predictor.train(X_.reshape(-1, 1), Y_.reshape(-1, 1), epochs=200)
+    lowerbound0, upperbound0, lowerbound1, upperbound1, P = Test(X0, X_, Y_, theta[[0]], predictor)
+    Draw(X_, Y_, X0, [loc[0]], theta[[0]], lowerbound0, upperbound0, lowerbound1, upperbound1)
